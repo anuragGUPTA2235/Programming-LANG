6c7bcdc29142c604f550187ca74456546a1c4660
Anurag Gupta <shekharanurag2235@gmail.com>
2024-07-13 18:35:51 +0530

    Update minimum_penalty.py

--- PYTHON/Accenture/minimum_penalty.py
+++ PYTHON/Accenture/minimum_penalty.py
@@ -1,1 +1,19 @@
+def min_penalty(num,arr):
+    arr.sort()
+    ans = 0
+    for i in range(len(arr)-1):
+         ans = ans + (arr[i+1] - arr[i])
+    return ans
+
+  
+
+
+
+
+if __name__ == "__main__":
+    num  = int(input())
+    arr = input()
+    arr = list(map(int,arr.split()))
+    ans = min_penalty(num,arr)
+    print(ans)
 
